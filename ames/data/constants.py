a505a3d340b5bc62e550c97cf95e7847f146ac9c
necromuralist <necromuralist@pm.me>
2020-03-15 15:47:34 -0700

    Added exploratory plots

--- ames/data/constants.py
+++ ames/data/constants.py
@@ -2,8 +2,12 @@ from argparse import Namespace
 
 
 RawData = Namespace(
+    parent_folder="RAW_DATA",
+    test="TEST_DATA",
+    submissions="SUBMISSIONS",
     environment_variable="TRAINING",
     target="SalePrice",
+    primary_key="Id",
     random_seed=0,
     train_size=0.8,
     test_size=0.2,
